aefede37ff77fa2db5622f12a396529cb4c53b98
Andrew Nhangud <Andrewnhangud3785@gmail.com>
2026-04-05 12:09:06 +0200

    feat: navbar added

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,64 @@
+import "./Navbar.css";
+import { useState } from "react";
+import closeIcon from "../../assets/Icons/close.svg";
+import menuIcon from "../../assets/Icons/menu.svg";
+
+export const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false); // sets the icons to false on the first render
+
+  // nav links for the navbar menu
+  const navLinks = [
+    { id: "about", label: "ABOUT" },
+    { id: "servics", label: "SERVICES" },
+    { id: "visit", label: "VISIT" },
+    { id: "contact", label: "CONTACT" },
+  ];
+
+  const handleOpen = () => setIsOpen((prev) => !prev); // keeps track of the icons when pressed
+  const handleClick = () => setIsOpen(false); // removes the menu if a nav link is pressed
+
+  return (
+    <nav className="global-container">
+      {/* navbar container */}
+      <div className="nav-container">
+        <h1>
+          <a className="logo" href="home">
+            Khumo Gardens
+          </a>
+        </h1>
+
+        {/* mobile menu icons  */}
+        <img
+          className="menu-icons remove-icons"
+          src={isOpen ? closeIcon : menuIcon}
+          onClick={handleOpen}
+          alt="menu icons"
+        />
+
+        {/* desktop menu */}
+        <ul className="desktop-menu">
+          {navLinks.map((link) => (
+            <li key={link.id}>
+              <a href={`#{link.id}`} onClick={handleClick}>
+                {link.label}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      {/* mobile menu */}
+      {isOpen && (
+        <ul className="mobile-menu">
+          {navLinks.map((link) => (
+            <li key={link.id}>
+              <a href={`#${link.id}`} onClick={handleClick}>
+                {link.label}
+              </a>
+            </li>
+          ))}
+        </ul>
+      )}
+    </nav>
+  );
+};
