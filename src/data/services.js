5a0dbe257ccb96729291fd11665881d743f2b159
Andrew Nhangud <Andrewnhangud3785@gmail.com>
2026-04-10 20:11:19 +0200

    added folder for data

---
+++ src/data/services.js
@@ -0,0 +1,39 @@
+import parkIcon from "../assets/Icons/park-picnic.svg";
+import photoShoot from "../assets/Icons/photo-camera.svg";
+import deco from "../assets/Icons/paint.svg";
+import wedding from "../assets/Icons/wedding-relationships.svg";
+import accommodation from "../assets/Icons/hotel.svg";
+import hosting from "../assets/Icons/private-house.svg";
+
+export const services = [
+  {
+    image: parkIcon,
+    title: "Day Visits",
+    para: "Take a break from the daily grind and come hang out in our gardens. Whether you're bringing the whole family, catching up with friends, or just need a little 'me time' in the quiet, we’ve got a spot waiting for you",
+  },
+  {
+    image: wedding,
+    title: "wedding",
+    para: "Imagine saying 'I do' tucked away in the beauty of nature. Our gardens aren't just a backdrop; they’re a romantic, living space designed to make your wedding day feel as magical as it looks",
+  },
+  {
+    image: photoShoot,
+    title: "Photo Shoot",
+    para: "Find your next great shot right here. From sweeping landscapes to hidden, sun-drenched corners, our gardens are designed to spark your creativity and help you capture something truly special.",
+  },
+  {
+    image: deco,
+    title: "Deco Services",
+    para: "Let’s bring your vision to life. Our team creates custom decor that doesn't just sit in the garden—it feels like a natural part of it, highlighting the beauty already around us.",
+  },
+  {
+    image: accommodation,
+    title: "Accommodation",
+    para: "Why rush off? Stay a little longer in a room that feels like home, only better. There’s nothing quite like waking up to birdsong and the sweet smell of the garden drifting through your window",
+  },
+  {
+    image: hosting,
+    title: "Private Functions",
+    para: "Whether you’re blowing out candles with family or getting the team together for a fresh perspective, our gardens offer a private, quiet space that’s all yours. We love helping you turn a simple gathering into a milestone",
+  },
+];
