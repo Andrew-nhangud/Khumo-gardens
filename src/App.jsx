9b26db1aaa90fc65932025c51bea34df2b0efc4e
Andrew Nhangud <Andrewnhangud3785@gmail.com>
2026-04-15 10:34:14 +0200

    calling the components to the main app

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,24 @@
 import "./Styles/global.css";
+import { Navbar } from "./Components/Navbar/Navbar";
+import { Hero } from "./Components/Hero/Hero";
+import { About } from "./Components/About/About";
+import { Services } from "./Components/Services/Services";
+import { Visit } from "./Components/Visit/Visit";
+import { Contact } from "./Components/Contact/Contact";
 
-function App() {}
+function App() {
+  return (
+    <div className="global-container">
+      <Navbar />
+      <main>
+        <Hero />
+        <About />
+        <Services />
+        <Visit />
+        <Contact />
+      </main>
+    </div>
+  );
+}
 
 export default App;
